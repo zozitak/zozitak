072aa1876285c4103f40ff93ef481a2315625f17
Zoltan Gabor Takacs <zozitak@gmail.com>
2020-11-05 06:57:05 +0100

    test to page

--- js/index.js
+++ js/index.js
@@ -1,15 +1,21 @@
-/* Set the width of the side navigation to 15% */
 function toggleNav() {
-  if ( document.getElementById("mysidenav").style.width != "0px" ) {
-  document.getElementById("mysidenav").style.width = "0px";
-  document.getElementById("togglebutton").style.left = "0px";
-  } else { 
-  document.getElementById("mysidenav").style.width = "15%";
-  document.getElementById("togglebutton").style.left = "15%";
-  }  
-}
+  let root = document.documentElement;
+  var myval = getComputedStyle(root)
+    .getPropertyValue('--side-navigation-bar-fix-width');
+  var myval2 = getComputedStyle(document.documentElement).getPropertyValue('--side-navigation-bar-width');
+  console.log(myval2);
 
-/* Set the width of the side navigation to 0 */
-function closeNav() {
-  document.getElementById("mysidenav").style.width = "0";
-} 
+  if ( getComputedStyle(document.documentElement).getPropertyValue('--side-navigation-bar-width') != "0vw" ) {
+  root.style.setProperty('--side-navigation-bar-width',"0vw");
+  document.getElementById("nav-profilepic").style.opacity = 0;
+  document.getElementById("nav-links").style.opacity = 0;
+  document.getElementById("nav-socicons").style.opacity = 0;
+  //console.log('0');
+  } else {
+  root.style.setProperty('--side-navigation-bar-width',myval);
+  document.getElementById("nav-profilepic").style.opacity = 1;
+  document.getElementById("nav-links").style.opacity = 1;
+  document.getElementById("nav-socicons").style.opacity = 1;
+  //console.log('1');
+  }
+}
