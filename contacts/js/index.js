c94f4a546fb913cfeb0fdedff004e84c7175aeff
Takács Zoltán Gábor <zozitak@gmail.com>
2023-02-08 18:48:06 +0100

    s

--- contacts/js/index.js
+++ contacts/js/index.js
@@ -1,24 +1,3 @@
-function toggleNav() {
-  let root = document.documentElement;
-  var myval = getComputedStyle(root)
-    .getPropertyValue('--side-navigation-bar-fix-width');
-  var myval2 = getComputedStyle(document.documentElement).getPropertyValue('--side-navigation-bar-width');
-  console.log(myval2);
-
-  if ( getComputedStyle(document.documentElement).getPropertyValue('--side-navigation-bar-width') != "0vw" ) {
-  root.style.setProperty('--side-navigation-bar-width',"0vw");
-  document.getElementById("nav-profilepic").style.opacity = 0;
-  document.getElementById("nav-links").style.opacity = 0;
-  document.getElementById("nav-socicons").style.opacity = 0;
-  //console.log('0');
-  } else {
-  root.style.setProperty('--side-navigation-bar-width',myval);
-  document.getElementById("nav-profilepic").style.opacity = 1;
-  document.getElementById("nav-links").style.opacity = 1;
-  document.getElementById("nav-socicons").style.opacity = 1;
-  //console.log('1');
-  }
-}
 document.body.addEventListener("keydown", function (event) {
     	if (event.key === "h") {
         	window.location.assign("/");
