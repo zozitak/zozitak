6d3b11153463ac0dbce47e67806c90fa58f28ead
Takács Zoltán Gábor <zozitak@gmail.com>
2023-02-20 18:05:23 +0100

    feat: navigation animation

--- js/index.js
+++ js/index.js
@@ -1,45 +1,55 @@
-function toggleNav() {
-  let root = document.documentElement;
-  var myval = getComputedStyle(root)
-    .getPropertyValue('--side-navigation-bar-fix-width');
-  var myval2 = getComputedStyle(document.documentElement).getPropertyValue('--side-navigation-bar-width');
-  console.log(myval2);
+document.body.addEventListener("keydown", function (event) {
+  if (event.key === "h") {
+    window.location.assign("/");
+  }
+  if (event.key === "a") {
+    window.location.assign("/aboutme");
+  }
+  if (event.key === "c") {
+    window.location.assign("/contacts");
+  }
+  if (event.key === "j") {
+    window.location.assign("/job");
+  }
+  if (event.key === "p") {
+    window.location.assign("/projects");
+  }
+  if (event.key === "w") {
+    window.location.assign("/wiki");
+  }
+  if (event.key === "t") {
+    window.location.assign("/test");
+  }
+});
 
-  if ( getComputedStyle(document.documentElement).getPropertyValue('--side-navigation-bar-width') != "0vw" ) {
-  root.style.setProperty('--side-navigation-bar-width',"0vw");
-  document.getElementById("nav-profilepic").style.opacity = 0;
-  document.getElementById("nav-links").style.opacity = 0;
-  document.getElementById("nav-socicons").style.opacity = 0;
-  //console.log('0');
-  } else {
-  root.style.setProperty('--side-navigation-bar-width',myval);
-  document.getElementById("nav-profilepic").style.opacity = 1;
-  document.getElementById("nav-links").style.opacity = 1;
-  document.getElementById("nav-socicons").style.opacity = 1;
-  //console.log('1');
+const footer = document.querySelector("footer");
+var mouseinfooter = false;
+
+footer.addEventListener("mouseenter", enterListener);
+footer.addEventListener("mouseleave", leaveListener);
+
+function enterListener(event) {
+  mouseinfooter = true;
+  if (footer.classList.contains("hidden")) {
+    footer.classList.toggle("hidden");
+    footer.classList.toggle("visible");
   }
 }
 
-document.body.addEventListener("keydown", function (event) {
-    	if (event.key === "h") {
-        	window.location.assign("/");
-    	}
-	if (event.key === "a") {
-        	window.location.assign("/aboutme");
-    	}
-    	if (event.key === "c") {
-        	window.location.assign("/contacts");
-    	}
-	if (event.key === "j") {
-        	window.location.assign("/job");
-    	}
-	if (event.key === "p") {
-        	window.location.assign("/projects");
-    	}
-	if (event.key === "w") {
-        	window.location.assign("/wiki");
-    	}
-	if (event.key === "t") {
-        	window.location.assign("/test");
-    	}
-});
+function leaveListener(event) {
+  mouseinfooter = false;
+  if (footer.classList.contains("visible")) {
+    footer.classList.toggle("visible");
+    footer.classList.toggle("hidden");
+  }
+}
+
+window.onload = () => {
+  footer.classList.toggle("visible");
+  setTimeout(() => {
+    if (!mouseinfooter && footer.classList.contains("visible")) {
+      footer.classList.toggle("visible");
+      footer.classList.toggle("hidden");
+    }
+  }, 2000);
+};
